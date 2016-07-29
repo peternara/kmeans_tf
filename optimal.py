d54525ca292ea76ba65daf3e20643fe5e4199217
Xisco Sastre <xiscosastre@gmail.com>
2016-07-29 12:08:08 +0200

    force first gpu

--- optimal.py
+++ optimal.py
@@ -29,31 +29,32 @@ vector_values = np.loadtxt(sys.argv[1])
 vector_values = np.delete(vector_values, 0, 1)
 print("Total IO Time: %3.2fs" % float(time.time() - begin_io_time))
 
-vectors = tf.constant(vector_values)
-centroids = tf.Variable(tf.slice(tf.random_shuffle(vectors),
-                                 [0, 0], [num_clusters, -1]))
-expanded_vectors = tf.expand_dims(vectors, 0)
-expanded_centroids = tf.expand_dims(centroids, 1)
+with tf.device('/gpu:0'):
+    vectors = tf.constant(vector_values)
+    centroids = tf.Variable(tf.slice(tf.random_shuffle(vectors),
+                                     [0, 0], [num_clusters, -1]))
+    expanded_vectors = tf.expand_dims(vectors, 0)
+    expanded_centroids = tf.expand_dims(centroids, 1)
 
-distances = tf.reduce_sum(
-    tf.square(tf.sub(expanded_vectors, expanded_centroids)), 2)
-assignments = tf.argmin(distances, 0)
+    distances = tf.reduce_sum(
+        tf.square(tf.sub(expanded_vectors, expanded_centroids)), 2)
+    assignments = tf.argmin(distances, 0)
 
-means = tf.concat(0, [
-    tf.reduce_mean(
-        tf.gather(vectors,
-                  tf.reshape(
-                      tf.where(
-                          tf.equal(assignments, c)
-                      ), [1, -1])
-                  ), reduction_indices=[1])
-    for c in range(num_clusters)])
+    means = tf.concat(0, [
+        tf.reduce_mean(
+            tf.gather(vectors,
+                      tf.reshape(
+                          tf.where(
+                              tf.equal(assignments, c)
+                          ), [1, -1])
+                      ), reduction_indices=[1])
+        for c in range(num_clusters)])
 
-update_centroids = tf.assign(centroids, means)
-init_op = tf.initialize_all_variables()
+    update_centroids = tf.assign(centroids, means)
+    init_op = tf.initialize_all_variables()
 
-# with tf.Session('local') as sess:
-sess = tf.Session()
+config = tf.ConfigProto(allow_soft_placement=True)
+sess = tf.Session(config=config)
 sess.run(init_op)
 
 begin_time = time.time()
